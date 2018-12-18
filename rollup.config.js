4ac0b98b3b3deb4bdb30d87a1b82edd0bfd10322
Pooya Parsa <pyapar@gmail.com>
2018-12-18 21:41:44 +0330

    feat: add browser-friendly version

--- rollup.config.js
+++ rollup.config.js
@@ -4,12 +4,21 @@ import pkg from './package.json';
 export default {
 	input: 'src/index.ts',
 	output: [
-		{ file: pkg.main, format: 'umd', name: 'devalue' },
+		{ file: pkg.browser, format: 'umd', name: 'devalue' },
+		{ file: pkg.main, format: 'cjs' },
 		{ file: pkg.module, format: 'es' }
 	],
 	plugins: [
 		typescript({
 			typescript: require('typescript')
-		})
+		}),
+		{
+			generateBundle({ format }, bundle) {
+				if (format === 'umd') {
+					const _bundle = bundle[Object.keys(bundle)[0]]
+					_bundle.code =  _bundle.code.replace(/require\('consola'\)/, 'console')
+				}
+			}
+		}
 	]
 };
